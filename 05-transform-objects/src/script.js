c831f98abec04731cda55f79e9a4ebfc0d98e6b3
Nicky van Urk <nickyvanurk@gmx.com>
2023-01-13 01:38:44 +0100

    Replace cube with a group of cubes

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -10,23 +10,18 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.set(0.7, -0.6, 1)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = -1
+scene.add(group)
 
-/**
- * Scale
- */
-mesh.scale.set(2, 0.5, 0.5)
-
-/**
- * Rotation
- */
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
+group.add(cube1)
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
+cube2.position.x = -2
+group.add(cube2)
+const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }))
+cube3.position.x = 2
+group.add(cube3)
 
 /**
  * Axes helper
@@ -47,7 +42,6 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.set(0, 0, 3)
-camera.lookAt(mesh.position)
 scene.add(camera)
 
 /**
