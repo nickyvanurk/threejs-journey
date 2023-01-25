e5e38e95a13a4e3a36b3e7b17acba2091236dae2
Nicky van Urk <nickyvanurk@gmx.com>
2023-01-25 10:52:58 +0100

    Allow for viewport resize

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -27,6 +27,14 @@ const sizes = {
     height: window.innerHeight,
 }
 
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+})
+
 /**
  * Camera
  */
